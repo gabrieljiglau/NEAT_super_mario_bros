d225bf101cbea2a162a912d41a7f8f22bc4a80f5
jiglau gabriel <100293793+gabrieljiglau@users.noreply.github.com>
2024-04-22 23:43:11 +0300

    Update BuildingBlocks.py
    add innovation number and manual tests for the random initialization of the population

--- src/BuildingBlocks.py
+++ src/BuildingBlocks.py
@@ -3,6 +3,7 @@ Author: gabriel jiglau
 StartingDate: 2024-III-26
 Description: All the concepts I will be using, but fragmented into smaller parts
 """
+
 import numpy as np
 
 
@@ -14,34 +15,53 @@ class NodesNotConnectedException(Exception):
         return self.message
 
 
+class Connection:
+    def __init__(self, in_node_id: int, out_node_id: int, is_enabled: bool, innovation_number: int):
+        self.in_node_id = in_node_id  # Index of the in_node in the gene's node list
+        self.out_node_id = out_node_id  # Index of the out_node in the gene's node list
+        self.is_enabled = is_enabled
+        self.innovation_number = innovation_number
+
+    def __str__(self):
+        return f"Connection from Node {self.in_node_id} to Node {self.out_node_id} is enabled : {self.is_enabled}, " \
+               f"innovation_number: {self.innovation_number}"
+
+
+class Node:
+    _id_counter = 0  # Class-level counter for assigning unique IDs to nodes
+
+    def __init__(self, weight):
+        self.id = Node._id_counter
+        Node._id_counter += 1
+        self.weight = weight
+        self.connections = []
+
+    def __str__(self):
+        connections_str = ', '.join([str(conn) for conn in self.connections])
+        return f"Node(id={self.id}, weight={self.weight}, connections=[{connections_str}])"
+
+    @classmethod
+    def reset_id_counter(cls):
+        cls._id_counter = 0  # Reset the node ID counter for each new gene
+
+
 """
 innovation_number :
 i)a historical archive that keeps track of all the connections and nodes that have  been created across all generations;
 ii)ensures consistency across different genomes, even if their topologies are different
 """
 
+class InnovationCounter:
+    def __init__(self):
+        self.current_innovation_number = 0
 
-class Connection:
-    def __init__(self, in_node, out_node, is_enabled, innovation_number):
-        self.in_node = in_node
-        self.out_node = out_node
-        self.is_enabled = is_enabled
-        self.innovation_number = innovation_number
-
-    def __str__(self):
-        return f"Connection from Node {self.in_node} to Node {self.out_node} is enabled : {self.is_enabled}, " \
-               f"innovation_number: {self.innovation_number}"
+    def get_new_innovation_number(self):
+        self.current_innovation_number += 1
+        return self.current_innovation_number
 
 
-class Node:
-    def __init__(self, weight):
-        # the weight should be set randomly at first
-        self.weight = weight
-        self.connections = []
-
-    def __str__(self):
-        return f"Node(weight={self.weight})"
-
+# global innovation counter
+global_innovation_counter = InnovationCounter()
 
 """ 
 the gene has the nodes and the connections 
@@ -50,9 +70,16 @@ it's the graph-like data-structure that holds them together
 
 
 class Gene:
+    _id_counter = 0  # class-level counter for assigning unique IDs to genes
 
     def __init__(self, nodes=None, connections=None):
-        self.previous_innovation_numbers = []
+        self.id = Gene._id_counter
+        Gene._id_counter += 1  # Increment the counter for the next gene
+
+        self.previous_innovation_numbers = {}
+
+        # Reset the node ID counter for each new gene
+        Node.reset_id_counter()
 
         if nodes is None:
             self.nodes = []
@@ -63,86 +90,93 @@ class Gene:
         else:
             self.connections = connections
 
-    def are_nodes_connected(self, first_node: 'Node', second_node: 'Node') -> bool:
-        # connections are one directional
+    def are_nodes_connected(self, first_node_id: int, second_node_id: int) -> bool:
         for connection in self.connections:
-            if connection.in_node == first_node and connection.out_node == second_node:
+            if connection.in_node_id == first_node_id and connection.out_node_id == second_node_id:
                 return True
+
         return False
 
-    def is_connection_enabled(self, source_node: 'Node', target_node: 'Node') -> bool:
+    def is_connection_enabled(self, source_node_id: int, target_node_id: int) -> bool:
         for connection in self.connections:
-            if connection.in_node == source_node and connection.out_node == target_node:
+            if connection.in_node_id == source_node_id and connection.out_node_id == target_node_id:
                 return connection.is_enabled
         return False
 
-    def add_node_between_genes(self, first_node: 'Node', second_node: 'Node', node_to_add: 'Node',
+    def add_node_between_genes(self, first_node_id: int, second_node_id: int, node_to_add: 'Node',
                                previous_innovation_numbers):
 
-        if not self.are_nodes_connected(first_node, second_node):
-            raise NodesNotConnectedException(first_node, second_node)
+        if not self.are_nodes_connected(first_node_id, second_node_id):
+            raise NodesNotConnectedException(first_node_id, second_node_id)
 
-        # Check if the connection already exists in previous_innovation_numbers
-        existing_innovation_number = self.find_matching_connection(first_node, second_node, previous_innovation_numbers)
+        innovation_number_first_connection = self.find_matching_connection(first_node_id, second_node_id,
+                                                                           previous_innovation_numbers)
+        innovation_number_second_connection = self.find_matching_connection(second_node_id, node_to_add.id,
+                                                                            previous_innovation_numbers)
+        is_enabled = True
+        need_innovation_first = True
+        need_innovation_second = True
 
-        if existing_innovation_number:
-            # Use the existing innovation_number
-            is_enabled = True
-            first_connection = Connection(first_node, node_to_add, is_enabled, existing_innovation_number)
-            second_connection = Connection(node_to_add, second_node, is_enabled, existing_innovation_number)
-        else:
-            # Assign a new innovation_number
-            is_enabled = True
+        first_connection = None
+        second_connection = None
+
+        if innovation_number_first_connection:
+            first_connection = Connection(first_node_id, node_to_add.id, is_enabled,
+                                          innovation_number_first_connection.innovation_number)
+            need_innovation_first = False
+
+        if innovation_number_second_connection:
+            second_connection = Connection(node_to_add.id, second_node_id, is_enabled,
+                                           innovation_number_second_connection.innovation_number)
+            need_innovation_second = False
+
+        if need_innovation_first:
             new_innovation_number = self.get_new_innovation_number()
-            first_connection = Connection(first_node, node_to_add, is_enabled, new_innovation_number)
-            second_connection = Connection(node_to_add, second_node, is_enabled, new_innovation_number)
+            first_connection = Connection(first_node_id, node_to_add.id, is_enabled, new_innovation_number)
 
-        self.connections.append(first_connection)
-        self.connections.append(second_connection)
+        if need_innovation_second:
+            new_innovation_number = self.get_new_innovation_number()
+            second_connection = Connection(node_to_add.id, second_node_id, is_enabled, new_innovation_number)
+
+        if first_connection is not None:
+            self.connections.append(first_connection)
+
+        if second_connection is not None:
+            self.connections.append(second_connection)
 
         self.nodes.append(node_to_add)
 
         # Update the existing connections
         for connection in self.connections:
-            if connection.out_node == second_node:
-                connection.out_node = node_to_add
-            if connection.in_node == second_node:
-                connection.in_node = node_to_add
+            if connection.out_node_id == second_node_id:
+                connection.out_node_id = node_to_add.id
+            if connection.in_node_id == second_node_id:
+                connection.in_node_id = node_to_add.id
 
     @staticmethod
-    def find_matching_connection(in_node: 'Node', out_node: 'Node', previous_innovation_numbers):
-        for gene in previous_innovation_numbers:
-            for connection in gene.connections:
-                if connection.in_node == in_node and connection.out_node == out_node:
-                    return connection
+    def find_matching_connection(in_node_id: int, out_node_id: int, previous_innovation_numbers):
+        innovation_key = (in_node_id, out_node_id)
+        if innovation_key in previous_innovation_numbers:
+            innovation_number = previous_innovation_numbers[innovation_key]
+            return Connection(in_node_id, out_node_id, True, innovation_number)
         return None
 
-    def get_new_innovation_number(self):
-        max_innovation = 0
-        for connection in self.connections:
-            max_innovation = max(max_innovation, connection.innovation_number)
-        return max_innovation + 1
+    @staticmethod
+    def get_new_innovation_number():
+        return global_innovation_counter.get_new_innovation_number()
 
-    """
-    checks whether or not the connection exists in the topology;
-    then adds the innovation_number accordingly(reuses it, or assigns a new value)
-    """
+    def add_connection(self, first_node_id: int, second_node_id: int):
+        innovation_key = (first_node_id, second_node_id)
 
-    def add_connection(self, first_node: 'Node', second_node: 'Node'):
-        existing_connection = self.find_matching_connection(first_node, second_node, self.previous_innovation_numbers)
-
-        if existing_connection:
-            connection = Connection(first_node, second_node, True, existing_connection.innovation_number)
+        if innovation_key in self.previous_innovation_numbers:
+            innovation_number = self.previous_innovation_numbers[innovation_key]
         else:
-            connection = Connection(first_node, second_node, True, self.get_new_innovation_number())
+            new_innovation_number = self.get_new_innovation_number()
+            innovation_number = new_innovation_number
+            self.previous_innovation_numbers[innovation_key] = new_innovation_number
 
+        connection = Connection(first_node_id, second_node_id, True, innovation_number)
         self.connections.append(connection)
-
-    """
-    Check if a connection between two nodes already exists in previous_genes.
-    i)if it does: reuse the existing connection's innovation number;
-    ii)else: create a new connection with a new innovation number;
-    """
 
     def initialize_gene_randomly(self, number_of_nodes, under_one_threshold, previous_innovation_numbers):
 
@@ -157,23 +191,28 @@ class Gene:
 
         for i in range(number_of_nodes):
             first_node = node_list[i]
-            for j in range(number_of_nodes):
+            for j in range(i + 1, number_of_nodes):
                 second_node = node_list[j]
-                random_connection_value = np.random.uniform(0, 1)
 
-                if random_connection_value < under_one_threshold:
-                    # Check if the connection already exists in previous_genes
-                    existing_connection = self.find_matching_connection(first_node, second_node,
+                generated_threshold = np.random.uniform(0, 1)
+                is_enabled = True
+
+                if i != j and generated_threshold < under_one_threshold:
+                    existing_connection = self.find_matching_connection(first_node.id, second_node.id,
                                                                         previous_innovation_numbers)
 
                     if existing_connection:
-                        # Use the existing innovation_number
-                        connection = Connection(first_node, second_node, True, existing_connection.innovation_number)
+                        connection = Connection(first_node.id, second_node.id, is_enabled,
+                                                existing_connection.innovation_number)
                     else:
-                        # Assign a new innovation_number
-                        connection = Connection(first_node, second_node, True, self.get_new_innovation_number())
+                        new_innovation_number = self.get_new_innovation_number()
+                        connection = Connection(first_node.id, second_node.id, is_enabled,
+                                                new_innovation_number)
 
-                    # Add the connection to the gene's connections list
+                        # Update previous_innovation_numbers with the new connection
+                        innovation_key = (connection.in_node_id, connection.out_node_id)
+                        previous_innovation_numbers[innovation_key] = connection.innovation_number
+
                     self.connections.append(connection)
 
         # return instance of the class to allow method chaining
