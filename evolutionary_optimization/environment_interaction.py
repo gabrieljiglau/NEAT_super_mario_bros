d35e64544810fbb9ceb8a5171bb5eb9393dbbae3
gabrieljiglau <gabriel_jiglau@yahoo.com>
2025-02-08 16:34:38 +0200

    parallize NEAT

--- evolutionary_optimization/environment_interaction.py
+++ evolutionary_optimization/environment_interaction.py
@@ -1,150 +1,136 @@
-import numpy as np
+import os
 import cv2
 import neat
 import pickle
-import os
+import numpy as np
+import multiprocessing
 import warnings; warnings.warn = lambda *args,**kwargs: None
 import gym_super_mario_bros
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+from collections import deque
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT, RIGHT_ONLY
 from nes_py.wrappers import JoypadSpace
 
+"""
+This code is based on:
+Source: https://github.com/asarathy28/smb_neat/blob/main/SMB/simple_movement/simple_SMB.py
+Author: asarathy28
+License: (Check the repository for the applicable license)
+"""
+
+STACK_SIZE = 4
+frame_stack = deque(maxlen=STACK_SIZE)
 env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0')
-env = JoypadSpace(env, SIMPLE_MOVEMENT)
+env = JoypadSpace(env, RIGHT_ONLY)
 
 generation = 0
 
 best_individual_fitness = float('-inf')
 best_individual_stats = {"distance": 0, "time": 0, "fitness": 0}
 
-def eval_genomes(genomes, config):
+success_counter = multiprocessing.Value("i", 0)
+
+def stack_frames(stack, new_frame, is_new_episode):
+
+    if is_new_episode:
+        stack.clear()
+        for _ in range(STACK_SIZE):
+            stack.append(new_frame)
+    else:
+        stack.append(new_frame)
+
+    return np.stack(stack, axis=0)
+
+def eval_genome(genome, config):
+    """Evaluates a single genome in NEAT."""
     global generation, best_individual_fitness, best_individual_stats
-    generation += 1
 
-    log_file = "training_statistics.txt"
-    first_time = True
+    observation = env.reset()
+    neural_network = neat.nn.FeedForwardNetwork.create(genome, config)
 
-    total_distance = 0
-    total_fitness = 0
-    total_time = 0
+    width, height, color = env.observation_space.shape
+    width = int(width / 8)
+    height = int(height / 8)
 
-    with open(log_file, "a") as log:
-        if first_time:
-            log.write(f"Current generation: {generation} \n")
-            first_time = False
+    best_fitness = 0
+    genome.fitness = 0
+    frame = 0
+    counter = 0
+    xpos_max = 0
+    distance_traveled = 0
+    done = False
+    success = False  # Track if this genome succeeds
 
-        for gene_id, genome in genomes:
-            observation = env.reset()
-            neural_network = neat.nn.FeedForwardNetwork.create(genome, config)
+    while not done:
 
-            width, height, color = env.observation_space.shape
-            width = int(width / 8)
-            height = int(height / 8)
+        observation = preprocess(observation, width, height)
+        observation = observation / 255.0  # normalize to range [0,1]
+        stacked_observation = stack_frames(frame_stack, observation, is_new_episode=False)
+        image_array = stacked_observation.flatten()
 
-            best_fitness = 0
-            genome.fitness = 0
-            frame = 0
+        network_output = neural_network.activate(image_array)
+        action_index = np.argmax(network_output)
+
+        observation, reward, done, info = env.step(action_index)
+
+        rew = reward if not isinstance(reward, np.generic) else reward.item()
+
+        current_x = info.get('x_pos', 0)
+        if not hasattr(genome, 'prev_x'):
+            genome.prev_x = current_x
+
+        speed = current_x - genome.prev_x
+        genome.prev_x = current_x
+
+        """
+        fitness_bonus = max(current_x ** 1.8 - info.get('time') ** 1.5 +
+                            min(max(current_x-50, 0), 1) * 2500 + success * 1e6, 0.00001) * 1e-6
+        """
+
+        fitness_bonus = (current_x * (speed ** 2) * 1e-4) if speed > 0 else -0.1
+        genome.fitness += rew + fitness_bonus
+
+        if current_x == xpos_max:
+            genome.fitness -= 0.05
+
+        if current_x > xpos_max:
+            genome.fitness += 1
+            xpos_max = current_x
+
+        if genome.fitness > best_fitness:
+            best_fitness = genome.fitness
             counter = 0
-            xpos_max = 0
-            distance_traveled = 0
-            done = False
-            success = False
+        else:
+            counter += 1
 
-            while not done:
-                image_array = []
-                frame += 1
+        distance_traveled = current_x
+        if info.get('flag_get', False):  # Success if the flag is reached
+            success = True
+            done = True
 
-                # preprocess observation
-                observation = cv2.resize(observation, (width, height))
-                observation = cv2.cvtColor(observation, cv2.COLOR_BGR2GRAY)
-                observation = np.reshape(observation, (width, height))
-                image_array = convert_matrix_into_array(observation, image_array)
+        life = info.get('life', 0)
+        if (life < 2 and genome.fitness < 1500) or counter == 150:
+            done = True
 
-                network_output = neural_network.activate(image_array)
-                action_index = np.argmax(network_output)
+    if genome.fitness > best_individual_fitness:
+        best_individual_fitness = genome.fitness
+        best_individual_stats = {
+            "distance": distance_traveled,
+            "time": frame,
+            "fitness": genome.fitness,
+        }
+        print(best_individual_stats)
 
-                # previously with 3 output nodes as binary outputs
-                """
-                network_output = [max(0, min(1, x)) for x in network_output]
-                binary_string = "".join(str(round(x)) for x in network_output)
-                action_index = int(binary_string, 2)
-                if action_index == 7:
-                    action_index = 0
-                """
+    if success:
+        with success_counter.get_lock():
+            success_counter.value += 1
 
-                observation, reward, done, info = env.step(action_index)
+    return genome.fitness  # Only return fitness
 
-                # update fitness
-                rew = reward if not isinstance(reward, np.generic) else reward.item()
-                genome.fitness += rew
-
-                xpos = info.get('x_pos', 0)
-
-                # penalize lack of progress
-                if xpos == xpos_max:
-                    genome.fitness -= 0.01
-
-                if xpos > xpos_max:
-                    genome.fitness += 1
-                    xpos_max = xpos
-
-                if genome.fitness > best_fitness:
-                    best_fitness = genome.fitness
-                    counter = 0
-                else:
-                    counter += 1
-
-                distance_traveled = xpos
-                if info.get('flag_get', False):  # success if the flag reached
-                    success += 1
-                    done = True
-
-                # end if life is lost or counter exceeds the threshold
-                life = info.get('life', 0)
-                if life < 2 or counter == 150:
-                    done = True
-
-            # update per-generation statistics
-            time_taken = frame
-            total_distance += distance_traveled
-            total_fitness += genome.fitness
-            total_time += time_taken
-
-            # check for the best individual
-            if genome.fitness > best_individual_fitness:
-                best_individual_fitness = genome.fitness
-                best_individual_stats = {
-                    "distance": distance_traveled,
-                    "time": time_taken,
-                    "fitness": genome.fitness,
-                }
-
-            print_info(gene_id, genome.fitness, info)
-
-        # statistics every 2 generations
-        if generation % 2 == 0:
-            mean_distance = total_distance / len(genomes)
-            mean_fitness = total_fitness / len(genomes)
-            mean_time = total_time / len(genomes)
-
-            log.write(f"\n=== Statistics for Generation {generation} ===\n")
-            log.write(f"Mean Distance: {mean_distance:.2f}\n")
-            log.write(f"Mean Fitness: {mean_fitness:.2f}\n")
-            log.write(f"Mean Time: {mean_time:.2f}\n")
-            log.write(f"Best Individual - Distance: {best_individual_stats['distance']}, "
-                      f"Time: {best_individual_stats['time']}, "
-                      f"Fitness: {best_individual_stats['fitness']}\n\n")
-            log.flush()
-
-        # success rate for this generation
-        log.write(f"Success rate: {success / len(genomes):.2f}\n")
-
-
-def convert_matrix_into_array(observation, image_array):
-    for x in observation:
-        for y in x:
-            image_array.append(y)
-
-    return image_array
+def preprocess(ob, inx, iny):
+    ob = cv2.resize(ob, (inx, iny))
+    ob = cv2.cvtColor(ob, cv2.COLOR_BGR2GRAY)
+    ob = np.reshape(ob, (inx, iny))
+    return ob
 
 def print_info(gene_id: int, gene_fitness: float, info: dict) -> None:
     print('END OF GENOME: ', gene_id)
@@ -156,6 +142,8 @@ def print_info(gene_id: int, gene_fitness: float, info: dict) -> None:
 
 def run_mario(config_file, total_iterations: int = 50):
 
+    num_cores = multiprocessing.cpu_count()
+
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet,
                          neat.DefaultStagnation, config_file)
     population = neat.Population(config)
@@ -166,8 +154,18 @@ def run_mario(config_file, total_iterations: int = 50):
 
     global generation
 
-    for gen in range(1, total_iterations + 1):
-        population.run(eval_genomes, 1)
+    # parallel evaluation is already implemented in NEAT via ParallelEvaluator
+
+    parallel_evaluator = neat.ParallelEvaluator(num_cores, eval_genome)
+
+    for generation in range(1, total_iterations + 1):
+        print(f"Now in generation {generation} inside NEAT")
+        population.run(parallel_evaluator.evaluate, total_iterations)
+
+        total_genomes = len(population.population)
+        success_rate = success_counter.value / total_genomes
+
+        print(f"Generation {generation}: Success Rate = {success_rate:.2%}")
 
         # save every 100 generations
         if generation % 100 == 0:
@@ -175,14 +173,17 @@ def run_mario(config_file, total_iterations: int = 50):
             best_genome = stats.best_genomes(1)[0]
             save_winner(best_genome, generation)
 
+        with success_counter.get_lock():
+            success_counter.value = 0
+
     winner = stats.best_genomes(1)[0]
-    with open('../../models/final_winner', 'wb') as output:
+    with open('../models/final_winner', 'wb') as output:
         pickle.dump(winner, output, 1)
-    return stats.best_genomes(1)[0]
+    return winner.fitness
 
 def save_winner(genome, generation_number):
     """Save the winner genome to a file."""
-    filename = f'../../models/winner_gen_{generation_number}.pkl'
+    filename = f'../models/winner_gen_{generation_number}.pkl'
     with open(filename, 'wb') as output:
         pickle.dump(genome, output, 1)
     print(f"Winner of generation {generation_number + 1} saved to {filename}.")
@@ -191,7 +192,7 @@ def save_winner(genome, generation_number):
 if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, "config")
-    new_config = 'config1'
+    # new_config = 'config1'
 
-    MAX_GENERATION_COUNT = 2000
+    MAX_GENERATION_COUNT = 500
     run_mario(config_path, MAX_GENERATION_COUNT)
